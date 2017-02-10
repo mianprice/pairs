5552abcabd371ed8e4f64cfdb62c82ee5af9d38b
Ian Price <michael.ian.price@gmail.com>
2017-02-10 11:05:35 -0500

    Program now saves total number of uses for each set to external file, displays them for the user, and allows user to choose to save their changes, leave the records unchanged, or reset the records to the initial values.

--- pairing.py
+++ pairing.py
@@ -1,3 +1,25 @@
+import os.path
+
+#Open record file
+filename = "saved_pairs.txt"
+contents = []
+if os.path.isfile(filename):
+    target = open(filename, 'r+')
+    c = list(target.read())
+    next_num = ""
+    for i in c:
+        if i != "\n":
+            if i == ",":
+                contents.append(int(next_num))
+                next_num = ""
+            else:
+                next_num += i
+else:
+    target = open(filename, 'w+')
+    for i in range(15):
+        contents.append(0)
+contents_backup = contents
+
 #Create list of possible values (one for each student)
 #Each character 0-f represents 1-16 in hexadecimal
 l = list("0123456789abcdef")
@@ -35,14 +57,18 @@ for i in range(len(l)-1):
     sets.append(s)
 
 #Ask which set the user would like to use, then print it
+print "There are 15 possible sets made up of 8 unique pairs each."
+for i in range(len(contents)):
+    print "Set #%d has been used %d times." % (i,contents[i])
+
 print """
-There are 15 possible sets made up of 8 unique pairs each.
 Which one would you like to use?
 Please enter an integer between 1 and 15, inclusive.\n
 """
-response = int(raw_input("Set #"))
+response1 = int(raw_input("Set #"))
 
-pair_set = sets[response-1]
+contents[response1 - 1] += 1
+pair_set = sets[response1 - 1]
 name_pairs = []
 
 for i in pair_set:
@@ -51,3 +77,33 @@ for i in pair_set:
         t.append(students[l.index(j)])
     name_pairs.append(t)
     print t
+
+#print "Are you done using the program?"
+#response2 = str(raw_input("(y/n):"))
+output_string = ""
+response2 ="y"
+if response2 == "y":
+    print "Do you want to save the records of how many times each set has been used?"
+    response3 = str(raw_input("(y/n):"))
+    if response3 == "y":
+        for i in contents:
+            output_string += str(i) + ","
+    elif response3 == "n":
+        print "Do you want to reset the number of times each set has been used?"
+        response4 = str(raw_input("(y/n):"))
+        if response4 == "y":
+            print "Resetting number of times each set has been used."
+            for i in range(15):
+                output_string += "0" + ","
+        elif response4 == "n":
+            print "Closing the program."
+            for i in contents_backup:
+                output_string += str(i) + ","
+elif response2 == "n":
+    print "Still in progress - will continue prompting"
+
+#Clearing records file, writing records, and closing the file
+target.seek(0)
+target.truncate()
+target.write(output_string)
+target.close()
